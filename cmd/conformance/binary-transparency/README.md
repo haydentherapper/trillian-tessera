c42d0b55e4a5e710d43c183d67df6a2a6d1f3fb0
Hayden B <hblauzvern@google.com>
2025-02-03 19:26:47 -0800

    wip

---
+++ cmd/conformance/binary-transparency/README.md
@@ -0,0 +1,56 @@
+# conformance-posix
+This binary runs an HTTP web server that accepts POST HTTP requests to an `/add` endpoint.
+This endpoint takes arbitrary data and adds it to a file-based log.
+
+## Bring up a log
+This will create a directory in your filesystem to store a log, and start a personality binary
+that can add entries to this log.
+
+First, define a few environment vaiables:
+
+```shell
+export LOG_PRIVATE_KEY="PRIVATE+KEY+example.com/log/testdata+33d7b496+AeymY/SZAX0jZcJ8enZ5FY1Dz+wTML2yWSkK+9DSF3eg"
+export LOG_PUBLIC_KEY="example.com/log/testdata+33d7b496+AeHTu4Q3hEIMHNqc6fASMsq3rKNx280NI+oO5xCFkkSx"
+export LOG_DIR=/tmp/mylog
+```
+
+Then, start the personality:
+
+```shell
+go run ./cmd/conformance/posix \
+  --storage_dir=${LOG_DIR} \
+  --initialise \
+  --listen=:2025 \
+  --v=2
+```
+
+## Add entries to the log
+### Manually
+Head over to the [codelab](../#codelab) to manually add entries to the log, and inspect the log.
+
+### Using the hammer
+In another terminal, run the [hammer](./internal/hammer) against the log.
+In this example, we're running 32 writers against the log to add 128 new leaves within 1 minute.
+
+```shell
+go run ./internal/hammer \
+  --log_public_key=example.com/log/testdata+33d7b496+AeHTu4Q3hEIMHNqc6fASMsq3rKNx280NI+oO5xCFkkSx \
+  --log_url=http://localhost:2025 \
+  --max_read_ops=0 \
+  --num_writers=32 \
+  --max_write_ops=64 \
+  --max_runtime=1m \
+  --leaf_write_goal=128 \
+  --show_ui=false
+```
+
+Optionally, inspect the log on the filesystem using the woodpecker tool to see the contents.
+Note that this reads only from the files on disk, so none of the commands above need to be running for this to work.
+
+```shell
+go run github.com/mhutchinson/woodpecker@main \
+  --custom_log_type=tiles \
+  --custom_log_url=file:///${LOG_DIR}/ \
+  --custom_log_origin=example.com/log/testdata \
+  --custom_log_vkey=${LOG_PUBLIC_KEY}
+```
---
+++ cmd/conformance/binary-transparency/main.go
@@ -0,0 +1,214 @@
+// Copyright 2024 The Tessera authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// posix runs a web server that allows new entries to be POSTed to
+// a tlog-tiles log stored on a posix filesystem. It allows to run
+// conformance/compliance/performance tests and showing how to use
+// the Tessera POSIX storage implmentation.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/mod/sumdb/note"
+
+	tessera "github.com/transparency-dev/trillian-tessera"
+	"github.com/transparency-dev/trillian-tessera/storage/posix"
+	"k8s.io/klog/v2"
+)
+
+var (
+	storageDir                = flag.String("storage_dir", "", "Root directory to store log data.")
+	initialise                = flag.Bool("initialise", false, "Set when creating a new log to initialise the structure.")
+	listen                    = flag.String("listen", ":2025", "Address:port to listen on")
+	privKeyFile               = flag.String("private_key", "", "Location of private key file. If unset, uses the contents of the LOG_PRIVATE_KEY environment variable.")
+	additionalPrivateKeyFiles = []string{}
+)
+
+func init() {
+	flag.Func("additional_private_key", "Location of addition private key, may be specified multiple times", func(s string) error {
+		additionalPrivateKeyFiles = append(additionalPrivateKeyFiles, s)
+		return nil
+	})
+}
+
+func addCacheHeaders(value string, fs http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", value)
+		fs.ServeHTTP(w, r)
+	}
+}
+
+// pkg:pypi/pandas@1.2.3
+type LogEntry struct {
+	PURL   string `json:"purl"`   // purl string, e.g. pkg:pypi/pandas@1.2.3
+	Digest string `json:"digest"` // hex-encoded sha256 digest
+}
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+	ctx := context.Background()
+
+	// Gather the info needed for reading/writing checkpoints
+	s, a := getSignersOrDie()
+
+	// Create the Tessera POSIX storage, using the directory from the --storage_dir flag
+	driver, err := posix.New(ctx, *storageDir, *initialise, tessera.WithCheckpointSigner(s, a...), tessera.WithBatching(256, time.Second))
+	if err != nil {
+		klog.Exitf("Failed to construct storage: %v", err)
+	}
+	addFn, _, err := tessera.NewAppender(driver, tessera.InMemoryDedupe(256))
+	if err != nil {
+		klog.Exit(err)
+	}
+
+	// Stores mapping between purl and sha256:digest
+	// TODO: Make persistent
+	lookupMap := make(map[string]string)
+
+	// Define a handler for /add that accepts POST requests and adds the POST body to the log
+	http.HandleFunc("POST /add", func(w http.ResponseWriter, r *http.Request) {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Create entry
+		var e LogEntry
+		if json.Unmarshal(b, &e); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		_, ok := lookupMap[e.PURL]
+		if ok {
+			w.WriteHeader(http.StatusConflict)
+			_, _ = w.Write([]byte("entry exists"))
+			return
+		}
+
+		// purl@@sha256:digest
+		leafData := fmt.Sprintf("%s@@sha256:%s", e.PURL, e.Digest)
+
+		idx, err := addFn(r.Context(), tessera.NewEntry([]byte(leafData)))()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+		if _, err = w.Write([]byte(fmt.Sprintf("%d", idx))); err != nil {
+			klog.Errorf("/add: %v", err)
+			return
+		}
+
+		// add digest lookup to map
+		lookupMap[e.PURL] = fmt.Sprintf("sha256:%s", e.Digest)
+	})
+
+	// we probably shouldn't have this, unless a language ecosystem has reproducible builds
+	http.HandleFunc("GET /lookup/{entry}", func(w http.ResponseWriter, r *http.Request) {
+		lookupKey := r.PathValue("entry")
+		if lookupKey == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte("lookup entry key missing"))
+			return
+		}
+
+		// TODO: verify purl spec adherence
+
+		val, ok := lookupMap[lookupKey]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		if _, err = w.Write([]byte(val)); err != nil {
+			klog.Errorf("/lookup/%s: %v", lookupKey, err)
+			return
+		}
+	})
+
+	// Proxy all GET requests to the filesystem as a lightweight file server.
+	// This makes it easier to test this implementation from another machine.
+	fs := http.FileServer(http.Dir(*storageDir))
+	http.Handle("GET /checkpoint", addCacheHeaders("no-cache", fs))
+	http.Handle("GET /tile/", addCacheHeaders("max-age=31536000, immutable", fs))
+	http.Handle("GET /", fs)
+
+	// TODO(mhutchinson): Change the listen flag to just a port, or fix up this address formatting
+	klog.Infof("Environment variables useful for accessing this log:\n"+
+		"export WRITE_URL=http://localhost%s/ \n"+
+		"export READ_URL=http://localhost%s/ \n", *listen, *listen)
+	// Run the HTTP server with the single handler and block until this is terminated
+	if err := http.ListenAndServe(*listen, http.DefaultServeMux); err != nil {
+		klog.Exitf("ListenAndServe: %v", err)
+	}
+}
+
+func getSignersOrDie() (note.Signer, []note.Signer) {
+	s := getSignerOrDie()
+	a := []note.Signer{}
+	for _, p := range additionalPrivateKeyFiles {
+		kr, err := getKeyFile(p)
+		if err != nil {
+			klog.Exitf("Unable to get additional private key from %q: %v", p, err)
+		}
+		k, err := note.NewSigner(kr)
+		if err != nil {
+			klog.Exitf("Failed to instantiate signer from %q: %v", p, err)
+		}
+		a = append(a, k)
+	}
+	return s, a
+}
+
+// Read log private key from file or environment variable
+func getSignerOrDie() note.Signer {
+	var privKey string
+	var err error
+	if len(*privKeyFile) > 0 {
+		privKey, err = getKeyFile(*privKeyFile)
+		if err != nil {
+			klog.Exitf("Unable to get private key: %q", err)
+		}
+	} else {
+		privKey = os.Getenv("LOG_PRIVATE_KEY")
+		if len(privKey) == 0 {
+			klog.Exit("Supply private key file path using --private_key or set LOG_PRIVATE_KEY environment variable")
+		}
+	}
+	s, err := note.NewSigner(privKey)
+	if err != nil {
+		klog.Exitf("Failed to instantiate signer: %q", err)
+	}
+	return s
+}
+
+func getKeyFile(path string) (string, error) {
+	k, err := os.ReadFile(path)
+	if err != nil {
+		return "", fmt.Errorf("failed to read key file: %w", err)
+	}
+	return string(k), nil
+}
